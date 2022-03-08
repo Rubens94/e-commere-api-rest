211a36b57d0edf549d94ef9a7c7e14dc6cb54040
Rubens94 <ruben9_1994@hotmail.com>
2022-03-07 22:23:43 -0600

    Create server

---
+++ README.md
@@ -0,0 +1,12 @@
+# E-commerce with NodeJS, ExpressJs and MongoDB
+
+## Install dependencies
+### npm install
+
+## Rename .example.env by .env
+
+## Production
+### npm start
+
+## Development
+### npm run test
---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000
+const MongoDB = process.env.MONGO_CNN
+
+// MongoDB
+mongoose
+    .connect(MongoDB)
+    .then(() => console.log('DB Connection Successfull!'))
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+// CORS
+app.use( cors() );
+
+// BODY Parser
+app.use( express.json() );
+
+// Routes
+app.use('/api/test', require('./routes/user'));
+
+app.listen(port, () => {
+    console.log(`Server runing on port: ${port}`);
+})
